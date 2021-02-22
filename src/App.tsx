267750a60b1a9c31e1c3467ed0cd18ec74ed1bb2
Felippe Vilas Boas <felippesantosvb@hotmail.com>
2021-02-22 20:41:25 -0300

    Day 1

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './styles/global.css'
+import './styles/responsive.css'
+import { ExperienceBar } from './components/ExperienceBar'
+
+function App() {
+  return (
+    <div className="container">
+      <ExperienceBar />
+    </div>
+  );
+}
+
+export default App;
